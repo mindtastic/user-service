2087ed7fe0390de3e6f707ca40bf0ac3315fda37
Bengisu Akkir <bengisuakkir@gmail.com>
2022-05-13 22:08:57 +0200

    added a seperate PyObjectId, and defined user Id in type integer

--- user_service/models/user_settings_model.py
+++ user_service/models/user_settings_model.py
@@ -1,14 +1,30 @@
 from typing import Optional
 from pydantic import BaseModel, Field
 from enum import Enum
+from bson import ObjectId
 
 class LanguageEnum(str, Enum):
     de = 'de'
     en = 'en'
 
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="integer")
 
 class UserSettingsSchema(BaseModel):
-    userId : int = Field(..., description="The user id")
+    id : PyObjectId = Field(alias="_id", default_factory=PyObjectId)
+    userId : int = Field(...)
     language: LanguageEnum = LanguageEnum.de #Default value is "de"
     #TODO more settings to be added here
     
@@ -16,6 +32,7 @@ class UserSettingsSchema(BaseModel):
         allow_population_by_field_name = True
         use_enum_values = True
         arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
         schema_extra = {
             "example": {
                 "userId": 1,
@@ -25,14 +42,17 @@ class UserSettingsSchema(BaseModel):
 
 #TODO check if this is needed
 class UpdateUserSettingsModel(BaseModel):
+    userId : int = Field(...)
     language: Optional[LanguageEnum]
     #more settings to be added here
     
     class Config:
         use_enum_values = True
         arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
         schema_extra = {
             "example": {
+                "userId": 1,
                 "language": "de"
             }
         }
