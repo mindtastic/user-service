5ea7510d206792d9ccdb3e5d6d1cd5eceb7ebcd1
Bengisu Akkir <bengisuakkir@gmail.com>
2022-05-13 18:36:14 +0200

    imported database interaction functions and user_settings model for router

--- user-service/routes/user_settings.py
+++ user-service/routes/user_settings.py
@@ -1,2 +1,22 @@
 from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
+
+#import database functions from user-service/database.py
+from database import (
+    add_new_user_settings_by_id, 
+    retrieve_user_settings_by_id, 
+    update_user_settings_by_id, 
+    delete_user_settings_by_id,
+)
+
+#import schemas from user-service/models/user_settings_model.py
+from models.user_settings_model import (
+    UserSettingsSchema,
+    UpdateUserSettingsModel,
+    ResponseModel,
+    ErrorResponseModel,
+)
+
+#Create FastAPI router 
+router = APIRouter()
+
