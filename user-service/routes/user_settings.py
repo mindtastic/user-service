34547edf81dff013458e5c7e85ce50d6fd7a55d4
Bengisu Akkir <bengisuakkir@gmail.com>
2022-05-13 18:46:32 +0200

    Created POST and GET endpoints to add and retrieve user settings
    Unauthorized error to be added

--- user-service/routes/user_settings.py
+++ user-service/routes/user_settings.py
@@ -20,3 +20,32 @@ from models.user_settings_model import (
 #Create FastAPI router 
 router = APIRouter()
 
+#Create POST endpoint for adding new user settings
+#TODO add Unauthorized error
+@router.post("/", response_model=ResponseModel, status_code=200, response_description="User settings created")
+async def add_new_user_settings(user_settings: UserSettingsSchema):
+    user_settings_data = jsonable_encoder(user_settings)
+    if await add_new_user_settings_by_id(user_settings_data["userId"], user_settings_data):
+        return ResponseModel(
+            message="User settings successfully added",
+            status_code=200,
+        )
+    return ErrorResponseModel(
+        message="User settings could not be added",
+        status_code=500,
+    )
+
+# Create GET endpoint for retrieving user settings by user id
+#TODO add Unauthorized error    
+@router.get("/{user_id}", response_model=ResponseModel, status_code=200, response_description="User settings retrieved")
+async def retrieve_user_settings(user_id: str):
+    if user_settings := await retrieve_user_settings_by_id(user_id):
+        return ResponseModel(
+            message="User settings retrieved",
+            status_code=200,
+            data=user_settings,
+        )
+    return ErrorResponseModel(
+        message="User settings could not be retrieved",
+        status_code=500,
+    )
