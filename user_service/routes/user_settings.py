f071550ce8e4938e7ec5ba454772425770ef56cb
Bengisu Akkir <bengisuakkir@gmail.com>
2022-05-13 19:10:19 +0200

    fixed wrong folder directs

--- user_service/routes/user_settings.py
+++ user_service/routes/user_settings.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
 
 #import database functions from user-service/database.py
-from database import (
+from user_service.database import (
     add_new_user_settings_by_id, 
     retrieve_user_settings_by_id, 
     update_user_settings_by_id, 
@@ -10,7 +10,7 @@ from database import (
 )
 
 #import schemas from user-service/models/user_settings_model.py
-from models.user_settings_model import (
+from user_service.models.user_settings_model import (
     UserSettingsSchema,
     UpdateUserSettingsModel,
     ResponseModel,
