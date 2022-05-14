91afa5edc0b2fbf77aef1b116ab53398fb6e7326
Bengisu Akkir <bengisuakkir@gmail.com>
2022-05-14 16:46:25 +0200

    initial root endpoint test created and tested

---
+++ user_service/test/test_routes.py
@@ -0,0 +1,15 @@
+from http import client
+from fastapi.testclient import TestClient
+from user_service.app import app
+
+client = TestClient(app) #create test client
+
+#test root endpoint
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+#TODO test all endpoints
+
+#run tests with pytest, after "pip install pytest" and "pip install requests"
