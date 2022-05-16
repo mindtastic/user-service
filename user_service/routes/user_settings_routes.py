588b07b31bc4b46ee08e3e44a938ea6010eb4714
Isabel Schmuck <isabel.schmuck@dampsoft.de>
2022-05-16 16:01:22 +0200

    Remove correct setting route

--- user_service/routes/user_settings_routes.py
+++ user_service/routes/user_settings_routes.py
@@ -24,19 +24,15 @@ async def retrieve_user_settings_by_id(userId: int):
     user_settings = await user_settings_collection.find_one({"userId": userId})
     if user_settings:
         return user_settings
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User settings not found")   
-
-# Create PUT endpoint for updating user settings by user ID
-#TODO add Unauthorized error
-@router.post("/{userId}/settings", response_description="Update user settings by user id", response_model=UserSettingsSchema)
-async def update_user_settings_by_id(userId: int, user_settings: UpdateUserSettingsModel):
-    user_settings = jsonable_encoder(user_settings)
-    updated_user_settings = await user_settings_collection.update_one(
-        {"userId": userId}, {"$set": user_settings}
-    )
-    if updated_user_settings:
-        return JSONResponse(status.HTTP_200_OK)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User settings not found")
+
+#Create POST endpoint for adding new user settings
+#TODO add Unauthorized error
+@router.post("/{userId}/settings", response_description="Add new user settings", response_model=UserSettingsSchema)
+async def add_new_user_settings(user_settings: UserSettingsSchema = Body(...)):
+    user_settings = jsonable_encoder(user_settings)
+    await user_settings_collection.insert_one(user_settings)
+    return JSONResponse(status_code=status.HTTP_200_OK, detail="User settings created")
 
 # Create DELETE endpoint for deleting user settings by user ID
 #TODO add Unauthorized error
