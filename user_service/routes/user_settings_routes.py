3b70eb1c5466ffe02069ddca03ff80e52cd9c5cf
Bengisu Akkir <bengisuakkir@gmail.com>
2022-05-20 19:17:47 +0200

    fixed issues in create user settings endpoint

--- user_service/routes/user_settings_routes.py
+++ user_service/routes/user_settings_routes.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Body
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 #import user settings collection from user-service/database.py
 from user_service.database import (
@@ -24,7 +25,7 @@ async def get_user_settings_by_id(user_id: int):
     Get user settings by user id
     """
     #check if user exists
-    user = await users_collection.find_one({"userId": user_id})
+    user = await users_collection.find_one({"userId": str(user_id)}) #TODO change to user_id - after alignment with Isabel regarding the type
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     
@@ -40,21 +41,22 @@ async def get_user_settings_by_id(user_id: int):
 # Create POST endpoint for user settings by user id
 # TODO add Unauthorized error
 @router.post("/{user_id}/settings", response_description="Add user settings by user id", response_model=UserSettingsResponse)
-async def create_user_settings(user_id: int, user_settings: UserSettingsSchema):
+async def create_user_settings(user_id: int, user_settings: UserSettingsSchema = Body(...)):
     """
     Add user settings by user id
     """
     #check if user exists
-    user = await users_collection.find_one({"userId": user_id})
+    user = await users_collection.find_one({"userId": str(user_id)})
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     
-    #create user settings
-    user_settings.userId = user_id
-    user_settings_id = await user_settings_collection.insert_one(user_settings)
-    user_settings.id = str(user_settings_id.inserted_id)
+    #check that user settings do not exist
+    user_settings_exists = await user_settings_collection.find_one({"userId": user_id})
+    if user_settings_exists is not None:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User settings already exists")
     
-    #return user settings
+    user_settings = jsonable_encoder(user_settings)
+    await user_settings_collection.insert_one(user_settings)
     return JSONResponse(status_code=status.HTTP_200_OK)
 
 
@@ -66,7 +68,7 @@ async def delete_user_settings(user_id: int):
     Delete user settings by user id
     """
     #check if user exists
-    user = await users_collection.find_one({"userId": user_id})
+    user = await users_collection.find_one({"userId": str(user_id)})
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     
