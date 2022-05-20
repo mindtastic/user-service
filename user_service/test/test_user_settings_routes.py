c28c495773720b0f5ce0956781e65a0952aa133f
Bengisu Akkir <bengisuakkir@gmail.com>
2022-05-20 19:18:16 +0200

    added more endpoint tests

--- user_service/test/test_user_settings_routes.py
+++ user_service/test/test_user_settings_routes.py
@@ -8,12 +8,65 @@ import json
 #create test mongodb client
 client = TestClient(app)
 
-user_settings_data = {
-  "userId": 1,
-  "language": "de"
+USERDATA = {
+    "userId": "1",
+    "username": "test",
+    "email": "test@test.com",
+    "role": "user",
 }
 
-#testing class
+user_settings_data = {
+  "userId": 1,
+  "language": "de",
+}
+
+#create test user for user settings tests
+""" def test_create_user(self):
+    response = client.post("user/", json.dumps(USERDATA, default=str))
+    assert response.status_code == 200 """
+
+#test create user settings endpoint
+def test_create_user_settings():
+    response = client.post("user/1/settings", json.dumps(user_settings_data))
+    assert response.status_code == 200
+
+#test creating duplicate user settings
+def test_create_duplicate_user_settings():
+    response = client.post("user/1/settings", json.dumps(user_settings_data))
+    assert response.status_code == 409
+
+#test get /user/{id} endpoint
+def test_get_user_settings_by_id():
+    response = client.get("user/1/settings")
+    assert response.status_code == 200
+    assert response.json() == user_settings_data
+
+#test delete user settings endpoint
+def test_delete_user_settings():
+    response = client.delete("user/1/settings")
+    assert response.status_code == 200
+
+#test get user settings after deleting
+def test_get_user_settings_after_deleting():
+    response = client.get("user/1/settings")
+    assert response.status_code == 404
+
+#test create user settings for non existing user
+def test_create_user_settings_for_non_existing_user():
+    response = client.post("user/3/settings", json.dumps(user_settings_data))
+    assert response.status_code == 404
+
+#test get for non existing user
+def test_get_user_settings_by_id_for_non_existing_user():
+    response = client.get("user/3/settings")
+    assert response.status_code == 404
+
+#test delete for non existing user
+def test_delete_user_settings_for_non_existing_user():
+    response = client.delete("user/3/settings")
+    assert response.status_code == 404
+
+""" #testing class
 class TestUserSettingsRoutes(TestCase):
 
     @classmethod
@@ -25,6 +78,11 @@ class TestUserSettingsRoutes(TestCase):
     def tearDownClass(cls):
         disconnect(alias='test_db')
     
+    #create test user for user settings tests
+    def test_create_user(self):
+        response = client.post("user/", json.dumps(USERDATA, default=str))
+        assert response.status_code == 200
+
     #test create user settings endpoint
     def test_create_user_settings(self):
         response = client.post("user/1/settings", json.dumps(user_settings_data))
@@ -49,4 +107,4 @@ class TestUserSettingsRoutes(TestCase):
     #test delete for non existing user
     def test_delete_user_settings_for_non_existing_user(self):
         response = client.delete("user/3/settings")
-        assert response.status_code == 404
+        assert response.status_code == 404 """
