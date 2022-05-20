44f426bc6f8ae06dd0ad356fb640ccb3111f6f9f
isabel <isaschm@users.noreply.github.com>
2022-05-20 11:51:04 +0200

    Feature: Add DB validation (#21)
    * Add linting suggestions
    
    * Add validation tests
    
    * Add delete route test

--- user_service/routes/user_routes.py
+++ user_service/routes/user_routes.py
@@ -1,65 +1,96 @@
+from email.policy import default
 from typing import List
 from fastapi import APIRouter, Body, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from jsonschema import ValidationError
 
 #import user settings collection from user-service/database.py
 from user_service.database import (
-    users_collection
+    users_collection,
 )
 
 #import schemas from user-service/models/user_model.py
 from user_service.models.user_model import (
     UserModel,
     UpdateUserModel,
-    UserModelResponse
+    UserModelResponse,
 )
 
-#Create FastAPI router 
+#Create FastAPI router
 router = APIRouter()
 
 @router.get(
-  "/", response_description="Get all users.", response_model=List[UserModelResponse]
+    "/",
+    response_description="Get all users.",
+    response_model=List[UserModelResponse],
 )
 async def show_all_users():
-  users = []
-  async for user in users_collection.find():
-      users.append(user)
-  return users
+    users = []
+    async for user in users_collection.find():
+        users.append(user)
+    return users
 
 @router.post(
-  "/", response_description="Add new user", response_model=UserModelResponse
+    "/", response_description="Add new user", response_model=UserModelResponse
 )
 async def add_new_user(user: UserModel = Body(...)):
     user = jsonable_encoder(user)
-    await users_collection.insert_one(user)
-    return JSONResponse(status_code=status.HTTP_200_OK, detail="User created")
+    print(user["role"])
+    try:
+        await users_collection.insert_one(user)
+    except ValidationError as error:
+        return HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail= error,
+    )
+
+    return JSONResponse(status_code=status.HTTP_200_OK)
 
 @router.get(
-    "/{userId}", response_description="Read user by id.", response_model=UserModelResponse
+    "/{userId}",
+    response_description="Read user by id.",
+    response_model=UserModelResponse,
 )
-async def show_user(userId: int):
-  if (user := await users_collection.find_one({"userId": userId})) is not None:
-      return user
+async def show_user(userId: str):
+    if (user := await users_collection.find_one({"userId": userId})) is not None:
+        return user
 
-  raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User {id} not found")
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"User {id} not found",
+    )
 
 @router.put(
-  "/{userId}", response_description="Update user by id.", response_model=UserModelResponse
+    "/{userId}",
+    response_description="Update user by id.",
+    response_model=UserModelResponse,
 )
-async def update_user(userId: int, user_data: UpdateUserModel = Body(...)):
-  user_data = jsonable_encoder(user_data)
-  updated_user_data = await users_collection.update_one(
-      {"userId": userId}, {"$set": user_data}
-  )
-  if updated_user_data:
-      return JSONResponse(status.HTTP_200_OK)
-  raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User {userId} not found")
+async def update_user(userId: str, user_data: UpdateUserModel = Body(...)):
+    if (await users_collection.find_one({"userId": userId})) is not None:
+        user_data = jsonable_encoder(user_data)
+        try:
+            await users_collection.update_one(
+                {"userId": userId}, {"$set": user_data}
+            )
+        except ValidationError as error:
+            return HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail= error,
+            )
+        return JSONResponse(status.HTTP_200_OK)
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User {userId} not found",
+    )
 
 @router.delete("/{userId}", response_description="Delete user by user id.")
-async def delete_user(userId: int):
+async def delete_user(userId: str):
     user = await users_collection.find_one({"userId": userId})
     if user:
         await users_collection.delete_one({"userId": userId})
         return JSONResponse(status.HTTP_200_OK)
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User {userId} not found")
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User {userId} not found",
+    )
--- user_service/routes/user_settings_routes.py
+++ user_service/routes/user_settings_routes.py
@@ -4,42 +4,58 @@ from fastapi.responses import JSONResponse
 
 #import user settings collection from user-service/database.py
 from user_service.database import (
-    user_settings_collection
+    user_settings_collection,
 )
 
 #import schemas from user-service/models/user_settings_model.py
 from user_service.models.user_settings_model import (
     UserSettingsSchema,
-    UpdateUserSettingsModel,
-    UserSettingsResponse
+    UserSettingsResponse,
 )
 
-#Create FastAPI router 
+#Create FastAPI router
 router = APIRouter()
 
 # Create GET endpoint for retrieving user settings by user ID
 # TODO add Unauthorized error
-@router.get("/{userId}/settings", response_description="Retrieve user settings by user id", response_model=UserSettingsResponse)
-async def retrieve_user_settings_by_id(userId: int):
-    user_settings = await user_settings_collection.find_one({"userId": userId})
+@router.get(
+    "/{userId}/settings",
+    response_description="Retrieve user settings by user id",
+    response_model=UserSettingsResponse,
+)
+async def retrieve_user_settings_by_id(user_id: int):
+    user_settings = await user_settings_collection.find_one({"userId": user_id})
     if user_settings:
         return user_settings
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User settings not found")
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User settings not found",
+    )
 
 #Create POST endpoint for adding new user settings
 #TODO add Unauthorized error
-@router.post("/{userId}/settings", response_description="Add new user settings", response_model=UserSettingsSchema)
+@router.post(
+    "/{userId}/settings",
+    response_description="Add new user settings",
+    response_model=UserSettingsSchema,
+)
 async def add_new_user_settings(user_settings: UserSettingsSchema = Body(...)):
     user_settings = jsonable_encoder(user_settings)
     await user_settings_collection.insert_one(user_settings)
-    return JSONResponse(status_code=status.HTTP_200_OK, detail="User settings created")
+    return JSONResponse(status_code=status.HTTP_200_OK)
 
 # Create DELETE endpoint for deleting user settings by user ID
 #TODO add Unauthorized error
-@router.delete("/{userId}/settings", response_description="Delete user settings by user id")
+@router.delete(
+    "/{userId}/settings",
+    response_description="Delete user settings by user id",
+)
 async def delete_user_settings_by_id(userId: int):
     user_settings = await user_settings_collection.find_one({"userId": userId})
     if user_settings:
         await user_settings_collection.delete_one({"userId": userId})
         return JSONResponse(status.HTTP_200_OK)
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User settings not found")
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User settings not found",
+    )
