167b5b38a3d257c2ba999abd1d262b483838d57e
Bengisu Akkir <bengisuakkir@gmail.com>
2022-06-06 16:18:26 +0200

    updated db connection to connect local db if env variables are empty

--- user_service/database.py
+++ user_service/database.py
@@ -3,15 +3,18 @@ import motor.motor_asyncio
 from urllib.parse import quote_plus
 
 MONGO_DETAILS = "mongodb://localhost:27017" # MongoDB details for local testing database
-user = os.getenv('MONGO_INITDB_ROOT_USERNAME', "admin")
-password = os.getenv('MONGO_INITDB_PASSWORD',  "test123")
-databaseHost = os.getenv('MONGODB_HOST', "mongodb_user_service")
+user = os.getenv('MONGO_INITDB_ROOT_USERNAME', "")
+password = os.getenv('MONGO_INITDB_PASSWORD',  "")
+databaseHost = os.getenv('MONGODB_HOST', "")
 uri = "mongodb://%s:%s@%s" % (quote_plus(user), quote_plus(password), databaseHost)
 
 #add try except block to handle if the database is not available
 try:
-    client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-    database = client.users # create new database called users
+    #check if the env variables are empty, if they are, use the local database
+    if user == "" or password == "":
+        client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+    else:
+        client = motor.motor_asyncio.AsyncIOMotorClient(uri)    
     print("Connected to MongoDB")
 except Exception as e:
     # raise configuration error
