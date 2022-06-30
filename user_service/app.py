f94c8ef384b60148cc641052598cf235b1214153
Bengisu Akkir <bengisuakkir@gmail.com>
2022-06-30 18:38:28 +0200

    added event handlers to app

--- user_service/app.py
+++ user_service/app.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI, status
 from user_service.routes.user_settings_routes import router as UserSettingsRouter
 from user_service.routes.user_routes import router as UsersRouter
+from user_service.events import create_startup_handler, create_shutdown_handler
 import logging
 
 PREFIX = "/user"
 
 # Create FastAPI app
 app = FastAPI()
+
+# Add Startup and Shutdown handlers
+app.add_event_handler('startup', create_startup_handler(app))
+app.add_event_handler('shutdown', create_shutdown_handler(app))
 
 #Add User Settings router
 app.include_router(UserSettingsRouter, prefix=PREFIX, tags=["User-Settings"])
