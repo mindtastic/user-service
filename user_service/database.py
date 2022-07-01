54d9aee32c7a18ce31514e96a1a14a2f83fdbc0b
Bengisu Akkir <bengisuakkir@gmail.com>
2022-07-01 16:41:34 +0200

    fix db connection

--- user_service/database.py
+++ user_service/database.py
@@ -1,21 +1,23 @@
+import logging
 import os
 from fastapi import FastAPI
 import motor.motor_asyncio
 from urllib.parse import quote_plus
 
-MONGO_DETAILS = "mongodb://localhost:27017" # MongoDB details for local testing database
-
 async def connect_to_mongodb(app: FastAPI):
-    uri = os.getenv("CONNECTION_STRING", connection_string_from_env())
+    connection_string = os.getenv("CONNECTION_STRING", None)
+    
     #add try except block to handle if the connection string is not set
     try:
         #check if the env variables are empty, if they are, use the local database
-        if uri is None:
-            client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
-            print("Connected to localhost:27017")
+        if connection_string is None:
+            buildin_connection_string = connection_string_from_env()
+            client = motor.motor_asyncio.AsyncIOMotorClient(buildin_connection_string)
+            logging.info(f"Connected to MongoDB from build-in connection string {buildin_connection_string}")
         else:
-            client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-            print("Connected to MongoDB database using env connection string")
+            client = motor.motor_asyncio.AsyncIOMotorClient(connection_string)
+            logging.info("Connected to MongoDB database using env connection string")
+        
         await client.admin.command("ismaster")
 
         database = client.users # create new database called users
@@ -30,7 +32,7 @@ def connection_string_from_env() -> str:
     return "mongodb://%s:%s@%s/%s?authSource=%s" % (
             quote_plus(os.getenv('MONGODB_USER', 'admin')),
             quote_plus(os.getenv('MONGODB_PASSWORD', 'admin')),
-            quote_plus(os.getenv('MONGODB_HOST')),
-            quote_plus(os.getenv('MONGODB_DB')),
+            quote_plus(os.getenv('MONGODB_HOST', 'mongo_user')),
+            quote_plus(os.getenv('MONGODB_DB', 'users')),
             quote_plus(os.getenv('MONGODB_AUTHSOURCE', 'admin'))
         )
