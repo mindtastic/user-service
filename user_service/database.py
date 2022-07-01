b8119bc884dee93db6db882ecde7916443079e5b
Isabel Schmuck <isabel.schmuck@dampsoft.de>
2022-07-01 17:21:39 +0200

    Add user_id idx

--- user_service/database.py
+++ user_service/database.py
@@ -22,7 +22,9 @@ async def connect_to_mongodb(app: FastAPI):
 
         database = client.users # create new database called users
         app.state.user_settings_collection = database.get_collection("user_settings_collection")
+        await app.state.user_settings_collection.create_index("user_id")
         app.state.user_collection = database.get_collection("users_collection")
+        await app.state.user_collection.create_index("user_id")
     except Exception as e:
         # raise configuration error
         logger.error("Error connecting to MongoDB: %s" % e)
